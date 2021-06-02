f66a3064ec648da1f8be69c90d9271aaa94ab999
Victor Freixinho <victorfn01@gmail.com>
2021-06-02 00:21:09 -0300

    Add eslint configs and load favicon with file-loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default function App(): React.ReactElement {
-  return (<div>Hello World!!!pudim</div>)
+  return (<div>Hello World!!!</div>)
 }
--- webpack.config.js
+++ webpack.config.js
@@ -5,23 +5,34 @@ module.exports = {
   entry: './src/index.jsx',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
-    publicPath: '/public/'
+    path: path.resolve(__dirname, '/build/js/'),
+    publicPath: '/public/static/js/',
   },
   module: {
-    rules:[{
+    rules: [{
       loader: 'babel-loader',
       test: /\.(js|jsx)$/,
-      exclude: /node_modules/
+      exclude: /node_modules/,
     },
-    { test: /\.tsx?$/, loader: 'ts-loader' }]
+    { test: /\.tsx?$/, loader: 'ts-loader' },
+    {
+      test: /\.jpe?g$|\.ico$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
+      loader: 'file-loader',
+      options: {
+        name: '/public/static/favicon/[name].[ext]',
+      },
+    }],
   },
   resolve: {
     extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
   },
   devServer: {
-    contentBase: path.resolve(__dirname, 'public'),
+    contentBase: [
+      path.join(__dirname, '/public/'),
+      path.join(__dirname, '/public/static/favicon/'),
+    ],
+    compress: true,
     port: 3000,
-    hot: true
-  }
+    hot: true,
+  },
 }
